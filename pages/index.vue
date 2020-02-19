b6cdc923fbd972e963ff1a30883ae0f9b3b2d4dd
Joao Brasil <joao_brasil@TSPD-NB-00475.tecnospeed.local>
2020-02-19 11:10:29 -0300

    ajustes na api

--- pages/index.vue
+++ pages/index.vue
@@ -27,25 +27,27 @@
         <v-toolbar-title>Aramazena Comprovante</v-toolbar-title>
       </v-app-bar>
 
-      <v-col cols="12">
-        <v-card>
-          <v-container fluid>
-            <v-row>
-              <v-col v-for="imagem in img" :key="imagem.id" class="d-flex child-flex" cols="4">
-                <v-card flat tile class="d-flex">
-                  <v-img :src="'https://apicomprovate.herokuapp.com/images/'+imagem.filename">
-                    <template v-slot:placeholder>
-                      <v-row class="fill-height ma-0" align="center" justify="center">
-                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                      </v-row>
-                    </template>
-                  </v-img>
-                </v-card>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card>
-      </v-col>
+      <v-content>
+        <v-container>
+          <v-card>
+            <v-container fluid>
+              <v-row>
+                <v-col v-for="imagem in img" :key="imagem.id" class="d-flex child-flex" cols="4">
+                  <v-card flat tile class="d-flex">
+                    <v-img :src="'https://apicomprovate.herokuapp.com/images/'+imagem.filename">
+                      <template v-slot:placeholder>
+                        <v-row class="fill-height ma-0" align="center" justify="center">
+                          <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                        </v-row>
+                      </template>
+                    </v-img>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
+        </v-container>
+      </v-content>
     </v-app>
   </div>
 </template>
